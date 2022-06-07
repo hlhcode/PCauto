c24f270357d8c4ccce87ada3ba5bc2734c87044d
41183 <1730440896@qq.com>
2022-06-07 19:00:30 +0800

    PC自动化打开方式修改

--- openapp.py
+++ openapp.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-from pywinauto import application
+from pywinauto import Application
 
-app = application.Application().start('notepad.exe')
-# app = application.Application().start('calc.exe')
-# app = application.Application().start('clock.exe')
+app= Application(backend='win32')
 
-chuangkou = '无标题-记事本'
-# print(app[chuangkou].print_control_identifiers())
+app.start('notepad.exe')
 
-app[chuangkou].Edit.type_keys('hahah\n',with_spaces=True,with_newlines=True)
-app[chuangkou].Edit.type_keys('nihao\n',with_spaces=True,with_newlines=True)
+app.connect(path='notepad.exe')
 
-app[chuangkou].menu_select
-    # ('文件->退出')
+app.kill()
