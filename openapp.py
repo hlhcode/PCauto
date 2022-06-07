7da1907f76e3509962b9ff6eacf12a938452dde1
41183 <1730440896@qq.com>
2022-06-07 19:02:21 +0800

    PC自动化打开方式修改

--- openapp.py
+++ openapp.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from pywinauto import Application
 
+# 打开应用
 app= Application(backend='win32')
 
+# 连接应用
 app.start('notepad.exe')
+
 
 app.connect(path='notepad.exe')
 
+# 关闭应用 https://blog.csdn.net/m0_37602827/article/details/108308991
 app.kill()
